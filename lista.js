966ae8e0ed8681d22711c1a1d530ae4b7650da17
gabriel22ramos <gabrielramoss232@gmail.com>
2025-03-16 13:55:28 -0300

    Lista de Tarefas

---
+++ lista.js
@@ -0,0 +1,25 @@
+let BotaoAdd = document.getElementById('botao');
+let Input = document.getElementById('input-tarefa');
+let Tarefas = document.getElementById('tarefas');
+let marcado = false;
+BotaoAdd.addEventListener('click', function() {
+    if (Input.value != '') {
+        var tarefa = document.createElement('p');
+        Tarefas.appendChild(tarefa);
+        tarefa.innerText = '- ' + Input.value;
+        Input.value = '';
+        tarefa.addEventListener('click', function () {
+            if (marcado == false) {
+                tarefa.style.textDecoration = 'line-through' 
+                marcado = true;
+            }
+            else {
+               tarefa.style .textDecoration = '';
+               marcado = false;
+            }
+        })
+        tarefa.addEventListener('dblclick', function () {
+            Tarefas.removeChild(tarefa);
+        })
+    }
+})
